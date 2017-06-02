7f86d6833c8cf3577333af869bbb44c7f2838051
user <user@ubuntu>
2017-06-02 02:31:11 -0400

    Satyam Creste user implemented

--- Documents/TeamWork/src/java/com/Paladion/teamwork/services/UserServiceImpl.java
+++ Documents/TeamWork/src/java/com/Paladion/teamwork/services/UserServiceImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Paladion.teamwork.services;
 
 import com.Paladion.teamwork.DAO.LoginDAO;
@@ -11,32 +6,16 @@ import com.Paladion.teamwork.beans.LoginBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-/**
- *
- * @author user
- */
 public class UserServiceImpl implements UserService{
 	
 @Autowired
 @Qualifier(value="UserDAO")
 UserDAO userDAO;
 	
-	@Override
+@Override
 	public void addUser(LoginBean loginBean) {
 		userDAO.addUser(loginBean);
-		System.out.println("com.Paladion.teamwork.services.UserServiceImpl.addUser()");
-import com.Paladion.teamwork.beans.UserBean;
+		System.out.println("com.Paladion.teamwork.services.UserServiceImpl.addUser()");	
 
-/**
- *
- * @author user
- */
-public class UserServiceImpl implements UserService{
-
-	@Override
-	public void addUser(UserBean ubean) {
-		
-		
-		}
-	
+}
 }
