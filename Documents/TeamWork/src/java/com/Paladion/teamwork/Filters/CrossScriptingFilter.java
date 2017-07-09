7e05beb231e1cbe8bbcdff54e16157c0380886cf
sumukh.r <sumukh.r@PAL-BLR-SUMU.PALBLR.NET>
2017-07-10 00:39:20 +0530

    sumukh XSS filter, response headers working but minor bug in CSRF filter

--- Documents/TeamWork/src/java/com/Paladion/teamwork/Filters/CrossScriptingFilter.java
+++ Documents/TeamWork/src/java/com/Paladion/teamwork/Filters/CrossScriptingFilter.java
@@ -17,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 public class CrossScriptingFilter implements Filter {
@@ -31,8 +32,16 @@ public class CrossScriptingFilter implements Filter {
         this.filterConfig = null;
     }
 
+        @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
         throws IOException, ServletException {
+        //cache control headers start
+        HttpServletResponse httpResp = (HttpServletResponse) response;
+        httpResp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpResp.setHeader("Pragma", "no-cache");
+        httpResp.setHeader("X-Frame-Options", "DENY");
+        httpResp.setHeader("Expires", "0");
+        //cache control headers end
     	logger.info("Inlter CrossScriptingFilter  ...............");
         chain.doFilter(new RequestWrapper((HttpServletRequest) request), response);
         logger.info("Outlter CrossScriptingFilter ...............");
